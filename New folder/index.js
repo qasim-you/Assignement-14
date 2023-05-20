9791af05086118786942ea2930d29233c40a826f
Qasim <qasimyousaf094@gmail.com>
2023-05-20 17:21:42 +0500

    initial commit

---
+++ New folder/index.js
@@ -0,0 +1,21 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  function deleteResult() {
+    document.getElementById('result').value = result.value.toString().slice(0,-1)
+  }
+  
+  function calculateResult() {
+    var result = document.getElementById('result').value;
+    try {
+      var answer = eval(result);
+      document.getElementById('result').value = answer;
+    } catch (error) {
+      document.getElementById('result').value = 'Error';
+    }
+  }
+  
